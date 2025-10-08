aad432e3737d4fdae7d478415bac36d6b77edad3
Pablo <rodrguezdelgado.pablo@gmail.com>
2025-10-08 12:29:22 +0200

    Añadida funcionalidad para actulizar la edad sin necesidad de darle al boton

--- app/src/main/java/com/example/mayoredad/MainActivity.kt
+++ app/src/main/java/com/example/mayoredad/MainActivity.kt
@@ -46,12 +46,14 @@ class MainActivity : AppCompatActivity() {
             val edad = numero.text.toString().toIntOrNull() ?: 0
             val nuevaEdad = edad + 1
             numero.setText(nuevaEdad.toString())
+            actualizarMensaje(nuevaEdad)
         }
 
         botonRestar.setOnClickListener {
             val edad = numero.text.toString().toIntOrNull() ?: 0
             val nuevaEdad = if (edad > 0) edad - 1 else 0
             numero.setText(nuevaEdad.toString())
+            actualizarMensaje(nuevaEdad)
         }
     }
 }
