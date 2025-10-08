d79728af1324a4503c44fbe381115419ac6e249b
Pablo <rodrguezdelgado.pablo@gmail.com>
2025-10-08 12:37:50 +0200

    actualizacion

--- app/src/main/java/com/example/mayoredad/MainActivity.kt
+++ app/src/main/java/com/example/mayoredad/MainActivity.kt
@@ -5,19 +5,10 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.ui.AppBarConfiguration
-import com.example.mayoredad.databinding.ActivityMainBinding
-
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
         val numero: EditText = findViewById(R.id.editTextText)
         val boton: Button = findViewById(R.id.button)
